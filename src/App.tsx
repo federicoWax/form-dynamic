1674399ff8740d0909ac460d1e1a2d7984cb8bd2
Federico <fcastillo@nomizor.com>
2022-01-25 12:55:17 -0700

    primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+interface Input {
+  id: number;
+  type: string;
+  label: string;
+  value: string | number | boolean;
+  options: null | Array<any>
+};
+
+const json: Input[] = [
+  {
+    id: 0,
+    type: "text",
+    label: "Input tipo texto",
+    value: "",
+    options: null
+  },
+  {
+    id: 1,
+    type: "select",
+    label: "Input tipo select",
+    value: "",
+    options: [
+      "Si"
+    ]
+  },
+  {
+    id: 2,
+    type: "radio",
+    label: "Input tipo radio",
+    value: false,
+    options: null
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hola
     </div>
   );
 }
