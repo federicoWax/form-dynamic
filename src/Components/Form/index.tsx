e0c708453dd885d5a0163567ef06b6a51cbb32c6
Fede <cha_lino_23@hotmail.com>
2022-01-25 14:52:56 -0700

    add input terminado

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,24 +1,37 @@
-import { Card, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField } from "@material-ui/core";
+import { Card, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField, Box, IconButton, Button } from "@material-ui/core";
 import { FC, useEffect, useState } from "react";
 import { InputInterface } from '../../Interfaces';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
 
 interface Props {
   json: Array<InputInterface>;
 }
 
-
+const initNewInput: InputInterface = {
+  id: 0,
+  type: "text",
+  label: "",
+  value: "",
+  options: []
+};
 
 const Form:FC<Props> = ({json}) => {
   const [values, setValues] = useState<Array<InputInterface>>([]);
+  const [formNewInput, setFormNewInput] = useState<InputInterface>(initNewInput);
+  const [label, setLabel] = useState<string>("");
+  const [loading, setLoading] = useState(true);
+  const [newValueInput, setNewValueInput] = useState<string>("");
 
   useEffect(() => {
+    if(!loading) return;
+    
     setValues(json);
-  }, [json]);
+    setLoading(false);
+  }, [json, loading]);
 
   const getInput = (input: InputInterface) => {
     const  { value, label, type, options } = input;
-
-    console.log(type)
 
     switch (type) {
       case "text":
@@ -60,15 +73,125 @@ const Form:FC<Props> = ({json}) => {
     }
   }
 
-  return <Card style={{margin: "10%", padding: 50}}>
-    {
-      values.map(input => {
-        return <div key={input.id} style={{margin: 20}}>
-          {getInput(input)}
-        </div>;
-      })
-    }
-  </Card>;
+  const deleteInput = (id: number) => {
+    setValues(values.filter(item => item.id !== id));
+  }
+
+  const addNewInput = () => {
+    if(!formNewInput.label) return;
+    if(["select", "radio"].includes(formNewInput.type) && !formNewInput.options?.length) return;
+
+    const lastItem = values[values.length - 1];
+    const id = lastItem ? lastItem.id + 1 : 0;
+
+    setValues([...values, {...formNewInput, id}]);
+    setFormNewInput(initNewInput);
+  }
+
+  const addValueInput = () => {
+    if(formNewInput.options?.includes(newValueInput)) return;
+
+    setFormNewInput({...formNewInput, options: [...formNewInput.options as Array<string>, newValueInput]});
+    setNewValueInput("");
+  }
+
+  const deletOptionNewInput = (value: string) => {
+    setFormNewInput({...formNewInput, options: formNewInput.options?.filter(item => item !== value) as Array<string> }) ;
+  }
+
+  return <Box sx={{ display: 'flex' }}>
+    <Box sx={{ width: '50%' }}>
+      <Card style={{marginLeft: "10%", marginRight: "10%", padding: 100}}>
+      {
+        values.map(input => {
+          return <div key={input.id} style={{margin: 50}}>
+            <Box sx={{ display: 'flex' }}>
+              <Box sx={{ width: '100%' }}>
+                {getInput(input)}
+              </Box>  
+              <Box>
+                <IconButton onClick={() => deleteInput(input.id)} style={{marginTop: 10}}>
+                  <DeleteIcon />
+                </IconButton>
+              </Box>
+            </Box>
+          </div>;
+        })
+      }
+      </Card>
+    </Box>
+    <Box sx={{ width: '50%' }}>
+      <Card style={{marginLeft: "10%", marginRight: "10%", padding: 100}}>
+        <Button variant="contained" style={{marginBottom: 20}} onClick={addNewInput}>
+          Agregar
+        </Button>
+        <TextField 
+          style={{marginBottom: 20}}
+          value={formNewInput.label}
+          label="Nombre del label" 
+          fullWidth 
+          onChange={e => setFormNewInput({...formNewInput, label: e.target.value})}
+          required
+          error={formNewInput.label.length === 0}
+        />
+        <FormControl 
+          fullWidth
+          style={{marginBottom: 20}}
+        >
+          <InputLabel>Tipo de input</InputLabel>
+          <Select
+            value={formNewInput.type}
+            label="Tipo de input"
+            onChange={e => setFormNewInput({...formNewInput, type: e.target.value as string})}
+          >
+            <MenuItem value="text">Texto</MenuItem>
+            <MenuItem value="select">Selector</MenuItem>
+            <MenuItem value="radio">Radio</MenuItem>
+          </Select>
+        </FormControl>
+        {
+          ["select", "radio"].includes(formNewInput.type) && <Card style={{padding: 20}}>
+            <Box sx={{ display: 'flex' }}>
+              <Box sx={{ width: '100%' }}>
+                Valores: { formNewInput.type === "select" ? "Selector" : "Radios" }
+                <TextField 
+                  style={{marginBottom: 20}}
+                  value={newValueInput}
+                  label="Valor" 
+                  fullWidth 
+                  onChange={e => setNewValueInput(e.target.value)}
+                />
+              </Box>  
+              <Box>
+                <IconButton onClick={() => addValueInput()} style={{marginTop: 10}}>
+                  <AddIcon />
+                </IconButton>
+              </Box>
+            </Box>
+            <br />
+            <div>
+            {
+              formNewInput.options?.map(option => 
+                <div key={option} style={{margin: 10}}>
+                  <Box sx={{ display: 'flex' }}>
+                    <Box sx={{ width: '100%' }}>
+                    {option}
+                    </Box>  
+                    <Box>
+                      <IconButton onClick={() => deletOptionNewInput(option)} style={{marginTop: 10}}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                </div>
+              )
+            }
+            </div>
+          </Card>
+        }
+      </Card>
+    </Box>
+  </Box>;
 };
 
 export default Form;
